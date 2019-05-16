ad4abd2a8e0fd1cb1e542d8d97ddf411bb540404
Chun-Chieh Liang <maple3312936@outlook.com>
2019-05-15 22:20:44 -0400

    android arch

---
+++ Architecture/app/src/main/java/com/chunchiehliang/kotlin/architecture/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.chunchiehliang.kotlin.architecture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
