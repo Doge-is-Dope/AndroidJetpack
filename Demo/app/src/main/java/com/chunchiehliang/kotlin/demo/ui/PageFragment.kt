026289fd9c19ce0649a84231433dcd7aa3e09125
Chun-Chieh Liang <maple3312936@outlook.com>
2019-06-12 16:55:11 -0400

    room

--- Demo/app/src/main/java/com/chunchiehliang/kotlin/demo/ui/PageFragment.kt
+++ Demo/app/src/main/java/com/chunchiehliang/kotlin/demo/ui/PageFragment.kt
@@ -7,10 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
 import com.chunchiehliang.kotlin.demo.R
 import com.chunchiehliang.kotlin.demo.databinding.FragmentPageBinding
+import com.google.android.material.tabs.TabLayout
 
 class PageFragment : Fragment() {
+    private lateinit var demoCollectionPagerAdapter: DemoCollectionPagerAdapter
+    private lateinit var viewPager: ViewPager
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding: FragmentPageBinding = DataBindingUtil.inflate(
@@ -18,9 +24,36 @@ class PageFragment : Fragment() {
             R.layout.fragment_page, container, false
         )
 
-
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        demoCollectionPagerAdapter = DemoCollectionPagerAdapter(childFragmentManager)
+        viewPager = view.findViewById(R.id.viewpager)
+        viewPager.adapter = demoCollectionPagerAdapter
+        val tabLayout: TabLayout = view.findViewById(R.id.tabs)
+        tabLayout.setupWithViewPager(viewPager)
+    }
 
+
+}
+
+class DemoCollectionPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(
+    fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    override fun getCount(): Int = 4
+
+    override fun getItem(i: Int): Fragment {
+        val fragment = ListFragment()
+        fragment.arguments = Bundle().apply {
+            // Our object is just an integer :-P
+//            putInt(ARG_OBJECT, i + 1)
+        }
+        return fragment
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return "tab ${(position + 1)}"
+    }
 }
