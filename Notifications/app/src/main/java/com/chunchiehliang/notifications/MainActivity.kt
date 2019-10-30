82cb6d9678aa6190907538b2118516c7f7a95b05
Chun-Chieh Liang <maple3312936@outlook.com>
2019-10-30 23:30:39 +0800

    notifications

--- Notifications/app/src/main/java/com/chunchiehliang/notifications/MainActivity.kt
+++ Notifications/app/src/main/java/com/chunchiehliang/notifications/MainActivity.kt
@@ -2,11 +2,18 @@ package com.chunchiehliang.notifications
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.chunchiehliang.notifications.ui.EggTimerFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, EggTimerFragment.newInstance())
+                .commitNow()
+        }
     }
 }
